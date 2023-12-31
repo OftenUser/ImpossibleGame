13b9b7f6b31fc84a28fcca14907f61c64c49e99b
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 12:49:48 -0500

    Update BufferedImageLoader.java

--- src/main/BufferedImageLoader.java
+++ src/main/BufferedImageLoader.java
@@ -6,13 +6,12 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class BufferedImageLoader {
-	
 	public BufferedImage loadImage(String pathRelativeToThis) throws IOException {
 		BufferedImage img = null;
 		try {
 			img = ImageIO.read(new FileInputStream(pathRelativeToThis));
-		} catch(Exception e) {
-			System.out.println("Couldn't find path: " + img);
+		} catch (Exception e) {
+			System.out.println("Couldn't Find Path: " + img);
 			System.out.println(pathRelativeToThis);
 		}
 		return img;
