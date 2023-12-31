6b4b79d7b76ddaa4dc3f1e1bbee813c6fcbf0218
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 13:28:00 -0500

    Update SpriteSheet.java

--- src/main/SpriteSheet.java
+++ src/main/SpriteSheet.java
@@ -3,11 +3,10 @@ package main;
 import java.awt.image.BufferedImage;
 
 public class SpriteSheet {
-	
 	public BufferedImage spriteSheet;
 	
-	public SpriteSheet(BufferedImage ss) {
-		this.spriteSheet = ss;
+	public SpriteSheet(BufferedImage spritesheet) {
+		this.spriteSheet = spritesheet;
 	}
 	
 	public BufferedImage grabSprite(int x, int y, int width, int height) {
